ab03ec5cd0ca23f4800aeb5dd3ffabef02f4a24b
sami <samiayachi653@gmail.com>
2024-12-06 07:28:22 +0100

    last commit :tada:

--- src/constants/index.js
+++ src/constants/index.js
@@ -39,248 +39,241 @@ const questions = [
     question:
       "Le corps humain est composé d'environ 60% d'eau, tout comme l'océan couvre environ 70% de la surface terrestre. Ainsi, polluer l'un revient à affecter un système vital.",
     answer: true,
-    image: { image1 },
+    image: image1,
   },
   {
     question:
       "Les déchets plastiques dans l’océan n’ont aucun impact sur les organismes marins, tout comme les toxines n’affectent pas le corps humain.",
     answer: false,
-    image: { image2 },
+    image: image2,
   },
   {
     question:
       "L’acidification de l’océan peut être comparée à un déséquilibre du pH dans le sang humain, mettant en danger le fonctionnement normal des systèmes internes.",
     answer: true,
-    image: { image3 },
+    image: image3,
   },
   {
     question:
       "Les microplastiques dans l’océan agissent exactement comme des nutriments essentiels dans le corps humain.",
     answer: false,
-    image: { image4 },
+    image: image4,
   },
   {
     question:
       "Lorsque l’océan est pollué, cela perturbe le cycle de vie marin, tout comme des toxines dans le corps perturbent le métabolisme humain.",
     answer: true,
-    image: { image5 },
+    image: image5,
   },
   {
     question:
       "Le corail dans l’océan joue un rôle similaire aux poumons dans le corps humain, filtrant et maintenant un équilibre vital.",
     answer: true,
-    image: { image6 },
+    image: image6,
   },
   {
     question:
       "La surpêche est à l’océan ce qu’un apport équilibré en nutriments est au corps humain, c’est-à-dire une action qui ne pose aucun problème.",
     answer: false,
-    image: { image7 },
+    image: image7,
   },
   {
     question:
       "Réduire la pollution des eaux n’a aucun lien avec la préservation de la santé humaine à long terme.",
     answer: false,
-    image: { image8 },
+    image: image8,
   },
   {
     question:
       "Les substances chimiques déversées dans l’océan peuvent être comparées aux polluants présents dans le sang humain, créant des dysfonctionnements dans les deux cas.",
     answer: true,
-    image: { image9 },
+    image: image9,
   },
   {
     question:
       "Le réchauffement des eaux océaniques n’a aucune conséquence sur les créatures marines, tout comme une forte fièvre est sans conséquence pour le corps humain.",
     answer: false,
-    image: { image10 },
+    image: image10,
   },
   {
     question:
       "La biodiversité marine diminue avec la pollution, tout comme la destruction de certains tissus réduit les capacités du corps humain.",
     answer: true,
-    image: { image11 },
+    image: image11,
   },
   {
     question:
       "Les marées noires dans l’océan n’affectent pas la chaîne alimentaire marine, un peu comme une intoxication n’affecterait pas la digestion humaine.",
     answer: false,
-    image: { image12 },
+    image: image12,
   },
   {
     question:
       "Le dioxyde de carbone absorbé par l’océan peut modifier sa chimie interne, un peu comme une mauvaise alimentation modifie la chimie interne du corps humain.",
     answer: true,
-    image: { image13 },
+    image: image13,
   },
   {
     question:
       "Les déchets rejetés en mer se décomposent instantanément sans affecter la faune, comme si les virus disparaissaient instantanément dans le corps humain.",
     answer: false,
-    image: { image14 },
+    image: image14,
   },
   {
     question:
       "L’accumulation de microplastiques dans la chaîne alimentaire marine est comparable à l’accumulation de toxines dans les tissus humains.",
     answer: true,
-    image: { image15 },
+    image: image15,
   },
   {
     question:
       "Un océan sain, riche en nutriments, est aussi essentiel à la planète qu’un régime équilibré l’est pour le corps humain.",
     answer: true,
-    image: { image16 },
+    image: image16,
   },
   {
     question:
       "La pollution sonore sous-marine n’a aucun effet sur les mammifères marins, comme le bruit excessif n’affecte pas l’oreille interne humaine.",
     answer: false,
-    image: { image17 },
+    image: image17,
   },
   {
     question:
       "La diminution de la quantité d’oxygène dans les eaux est comparable à une baisse d’oxygénation du sang dans le corps humain.",
     answer: true,
-    image: { image18 },
+    image: image18,
   },
   {
     question:
       "Les coraux ne sont pas affectés par la température ou la pollution de l’eau, tout comme le cœur humain n’est pas affecté par la qualité du sang qui y circule.",
     answer: false,
-    image: { image19 },
+    image: image19,
   },
   {
     question:
       "Le mercure dans les poissons accumulé par la pollution océanique est similaire aux métaux lourds que le corps humain peut accumuler, perturbant ainsi ses fonctions.",
     answer: true,
-    image: { image20 },
+    image: image20,
   },
   {
     question:
       "La présence de plastiques sur les plages n’influence en rien l’état des écosystèmes marins, tout comme la présence de toxines n’affecte pas la peau humaine.",
     answer: false,
-    image: { image21 },
+    image: image21,
   },
   {
     question:
       "Une eau de mauvaise qualité dans l’océan peut se comparer à un déséquilibre des fluides corporels dans l’organisme humain, nuisant à la santé globale.",
     answer: true,
-    image: { image22 },
+    image: image22,
   },
   {
     question:
       "L’ingestion de déchets par les animaux marins n’a aucun impact sur les prédateurs ultérieurs, contrairement à la bioaccumulation de toxines dans le corps humain.",
     answer: false,
-    image: { image23 },
+    image: image23,
   },
   {
     question:
       "La pollution par les engrais dans les eaux côtières provoque des zones mortes, un peu comme un excès de toxines provoque une défaillance organique dans le corps.",
     answer: true,
-    image: { image24 },
+    image: image24,
   },
   {
     question:
       "Les micro-organismes nocifs ne se multiplient pas dans un océan pollué, tout comme les bactéries nocives ne se multiplient pas dans un corps affaibli.",
     answer: false,
-    image: { image25 },
+    image: image25,
   },
   {
     question:
       "Le blanchiment des coraux à cause de la chaleur et de la pollution est similaire à la destruction de cellules essentielles dans le corps humain due à des toxines.",
     answer: true,
-    image: { image26 },
+    image: image26,
   },
   {
     question:
       "Les produits chimiques rejetés en mer disparaissent immédiatement sans laisser de traces, comme si les polluants sanguins disparaissaient instantanément dans le corps.",
     answer: false,
-    image: { image27 },
+    image: image27,
   },
   {
     question:
       "Un océan équilibré est à la planète ce qu’un microbiote équilibré est au corps humain : un facteur de bonne santé générale.",
     answer: true,
-    image: { image1 },
+    image: image1,
   },
   {
     question:
       "L’excès de nutriments dans l’eau de mer n’a aucun impact sur l’équilibre naturel des espèces, comme un excès de sucre n’a aucun effet sur la santé humaine.",
     answer: false,
-    image: { image2 },
+    image: image2,
   },
   {
     question:
       "Les nappes de pétrole perturbent les écosystèmes marins comme des toxines dans le sang perturbent les fonctions organiques humaines.",
     answer: true,
-    image: { image3 },
+    image: image3,
   },
   {
     question:
       "Des températures océaniques extrêmes n’ont aucune conséquence sur la vie marine, comme une forte fièvre n’a aucune conséquence sur le corps humain.",
     answer: false,
-    image: { image4 },
+    image: image4,
   },
   {
     question:
       "La pollution plastique crée des lésions internes chez les animaux marins, un peu comme des particules nocives peuvent endommager les organes internes chez l’homme.",
     answer: true,
-    image: { image5 },
+    image: image5,
   },
   {
     question:
       "Des déchets radioactifs dans l’océan sont totalement inoffensifs, comme des radiations seraient inoffensives dans le corps humain.",
     answer: false,
-    image: "https://www.climatecentral.org/media/ocean-acidification.jpg",
+    image: image6,
   },
   {
     question:
       "Un déséquilibre des courants marins lié à la pollution est comparable à une mauvaise circulation sanguine dans le corps humain.",
     answer: true,
-    image:
-      "https://passportocean.com/wp-content/uploads/2017/11/ocean-pollution.jpg",
+    image: image1,
   },
   {
     question:
       "L’augmentation de la salinité due à la pollution n’a aucun impact sur la vie marine, tout comme un déséquilibre en minéraux dans le sang n’affecte pas le métabolisme humain.",
     answer: false,
-    image:
-      "https://www.newsweek.com/heartbreaking-images-plastic-pollution-ocean-1459494",
+    image: image10,
   },
   {
     question:
       "Les barrières de corail endommagées par la pollution sont comparables à des organes vitaux endommagés dans le corps humain, compromettant la santé globale.",
     answer: true,
-    image:
-      "https://www.nationalgeographic.com/environment/article/ocean-pollution",
+    image: image10,
   },
   {
     question:
       "La pollution des eaux n’affecte que la surface de l’océan, comme une maladie n’affecterait que la surface de la peau humaine.",
     answer: false,
-    image:
-      "https://lotusarise.com/wp-content/uploads/2020/12/Ocean-Acidification.jpg",
+    image: image12,
   },
   {
     question:
       "L’accumulation de métaux lourds dans les poissons est similaire à l’accumulation de substances toxiques dans les tissus humains.",
     answer: true,
-    image:
-      "https://maritimecyprus.com/wp-content/uploads/2023/02/Ocean-Pollution.jpg",
+    image: image11,
   },
   {
     question:
       "La diminution du plancton liée à la pollution n’affecte pas la chaîne alimentaire marine, comme une carence en nutriments n’affecte pas la santé humaine.",
     answer: false,
-    image:
-      "https://www.nationalgeographic.com/environment/article/ocean-pollution",
+    image: image11,
   },
   {
     question:
       "Un océan sain favorise la régulation du climat, tout comme un corps en bonne santé régule sa température.",
     answer: true,
-    image:
-      "https://www.newsweek.com/heartbreaking-images-plastic-pollution-ocean-1459494",
+    image: image13,
   },
 ];
 
