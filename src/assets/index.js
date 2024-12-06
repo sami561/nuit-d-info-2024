dbc9845e166c1d1164039c7e84debec12490d02d
sami <samiayachi653@gmail.com>
2024-12-06 07:23:15 +0100

    add text

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,4 +5,66 @@ import logo from "./Logo.webp";
 import moon from "./moon.png";
 import leftArrow from "./left-arrow.svg";
 import rightArrow from "./right-arrow.svg";
-export { leftArrow, rightArrow, logo, moon, menu, close };
+import image1 from "./images/image1.jpg";
+import image2 from "./images/image2.jpg";
+import image3 from "./images/image3.jpg";
+import image4 from "./images/image4.jpg";
+import image5 from "./images/image5.jpg";
+import image6 from "./images/image6.avif";
+import image7 from "./images/image7.jpg";
+import image8 from "./images/image8.jpg";
+import image9 from "./images/image9.jpg";
+import image10 from "./images/image10.jpg";
+import image11 from "./images/image11.jpg";
+import image12 from "./images/image12.jpg";
+import image13 from "./images/image13.jpg";
+import image14 from "./images/image14.jpg";
+import image15 from "./images/image15.jpg";
+import image16 from "./images/image16.jpg";
+import image17 from "./images/image17.jpg";
+import image18 from "./images/image18.jpg";
+import image19 from "./images/image19.jpg";
+import image20 from "./images/image20.jpg";
+import image21 from "./images/image21.jpg";
+import image22 from "./images/image22.jpg";
+import image23 from "./images/image23.jpg";
+import image24 from "./images/image24.jpg";
+import image25 from "./images/image25.webp";
+import image26 from "./images/image26.jpg";
+import image27 from "./images/image27.jpg";
+
+export {
+  leftArrow,
+  rightArrow,
+  logo,
+  moon,
+  menu,
+  close,
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20,
+  image21,
+  image22,
+  image23,
+  image24,
+  image25,
+  image26,
+  image27,
+};
